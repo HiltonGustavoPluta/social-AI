9659f7dd40c8537649b8475050a5eb6c8e68c5c1
Hilton <htn.contato@hotmail.com>
2025-02-25 16:56:49 -0300

    chore: adjuste prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var cachedPrisma: PrismaClient | undefined;
+// Evita múltiplas instâncias do Prisma no ambiente de desenvolvimento
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const db = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
 }
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
-}
-
-export const db = prisma;
